dc638535e5c027873212651775e90152128338d9
Mohamed Hassanin <mhassani@mathworks.com>
2021-01-16 22:07:24 -0500

    tsest 4

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ async fn main() {
     println!("Hanlde result is {:?}", handle);
     // let set = res2.await;
     //soft
-    // print!("Result is {:?}", set);
+    // print!("Result is {:?}", set); sadsa
     // let res3 = res.and_then(|r4| r4 * 2);
 
     ()
